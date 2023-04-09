d8fc395f66bf212c9722eb49a3e501c9cb8c5763
Arbaaz Khan <arbaazkhanark23@gmail.com>
2023-04-10 02:55:08 +0530

    Winner List Complete Successfully

--- script.js
+++ script.js
@@ -22,6 +22,52 @@ function roll(event){
             document.querySelector(`#${spans[i].id}`).innerHTML=`${rand}`;
         }
     }
+}
 
+
+
+
+let resBtn=document.querySelector("#endButton");
+
+resBtn.addEventListener("click",result);
+
+
+function result(){
+    let arr=document.querySelectorAll("span");
+
+    let max=0;
+    for(let i=1;i<arr.length;i++){
+        if(arr[i].innerHTML>max){
+            max=arr[i].innerHTML;
+        }
+    }
+
+
+    let maxScorePlayer=[];
+    for(let i=1;i<arr.length;i++){
+        if(arr[i].innerHTML==max){
+            // console.log((arr[i].id));
+            console.log("resulting...");
+            maxScorePlayer.push(arr[i].id);
+        }
+    }
+
+
+
+    
+    let a=document.querySelectorAll("input");
+    // console.log("Winner:"+ maxScorePlayer)
+    let winners=[];
+    for(let i=0;i<a.length;i++){
+        for(let j=0;j<maxScorePlayer.length;j++){
+            if(a[i].className==maxScorePlayer[j]){
+                let winner=a[i].value;
+                winners.push(winner);
+            }
+        }
+    }
+    console.log(winners)
+    let final=winners.join();
+    document.querySelector("#winMessage").innerHTML="Winner is: "+final;
 
 }
