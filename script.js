a7b8b382f9d210642d665bce6977f77296f4b080
Arbaaz Khan <arbaazkhanark23@gmail.com>
2023-04-10 00:45:25 +0530

    roll function done

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+let btns=document.querySelectorAll(".dice");
 
-console.log("Checking for git...");
-console.log("pushing code to repo")
+// console.log(btns);
+
+for(let i of btns){
+    i.addEventListener("click",roll);
+}
+
+
+function roll(event){
+    console.log("rolling");
+    let turn=event.target.id;
+    // console.log(turn);
+    let rand=parseInt(Math.random()*7);
+    // console.log(rand);
+
+
+    let spans=document.querySelectorAll("span");
+    for(let i=1;i<spans.length;i++){
+        if(turn.toLowerCase()==(spans[i].id).toLowerCase()){
+            // console.log((spans[i].id).toLowerCase());
+            document.querySelector(`#${spans[i].id}`).innerHTML=`${rand}`;
+        }
+    }
+
+
+}
